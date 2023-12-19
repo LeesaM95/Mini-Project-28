8071e3bc3c54b607d25c8bd79efc3ebd18047df5
gblow <144559335+gblow@users.noreply.github.com>
2023-12-18 22:00:08 -0800

    Updated js and package
    complete JS code and updated the inquirer package file

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-function generateHTML(name) {
-    return ` <html> ${name} </html> `
-}
+// function generateHTML(name) {
+//     return ` <html> ${name} </html> `
+// }
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Where are you from?',
+      name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'What is your favorite food?',
+        name: 'food',
+      },
+      {
+        type: 'input',
+        message: 'What is your top future travel destination?',
+        name: 'travel',
+      },
+    
+    {
+      type: 'input',
+      message: 'What makes you unique?',
+      name: 'unique',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github URL?',
+        name: 'github',
+      },  {
+        type: 'input',
+        message: 'What is your LinkedIn URL',
+        name: 'linkedin',
+      },
+
+  ])
+  .then((response) => {
+    const htmlPage = generateHTML(response);
+      fs.writeFile('index.html', htmlPage, (err) =>
+      err? console.error(err) : console.log('Success')
+      );
+});
