90c62539fa639f579f475abcea788ca1f1916f98
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2023-12-19 00:30:00 -0500

    initial commit. started putting in js/html

--- index.js
+++ index.js
@@ -2,5 +2,26 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 function generateHTML(name) {
-    return ` <html> ${name} </html> `
+    return ` 
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Portfolio Generator</title>
+    <link rel="stylesheet" href="style.css" />
+    </head>
+    
+    <body>
+    <h1>Portfolio Generator</h1>
+    <div class="info">
+        <div class="name">${name}</div>
+        <div class="location">${location}</div>
+        <div class="bio">${Bio}</div>
+        <div class="linkedin">${linkedIn}:</div>
+        <div class="github">${gitHub}:</div>
+    </div>
+    <script src=".index.js"></script>
+    </body>
+    </html> `
 }
+
